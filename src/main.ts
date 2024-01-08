b47cf0164bdcc03a65393a8696a627f7288e4523
JoseEduardoRivera <81337741+JoseEduardoRivera@users.noreply.github.com>
2024-01-08 16:39:36 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,24 +9,15 @@ import "bootstrap"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey:process.env.API_KEY,
-//     authDomain:process.env.AUTH_DOMAIN,
-//     projectId:process.env.PROJECT_ID ,
-//     storageBucket:process.env.STORAGE_BUCKETS,
-//     messagingSenderId:process.env.MESSAGE_SENDING_ID,
-//     appId:process.env.APPID,
-//     measurementId:process.env.MEASUREMENT_ID
-//   };
 const firebaseConfig = {
-        apiKey: "AIzaSyAwsmAZZ2PG9y2dMMUiMZU0tZp7WGimwQc",
-        authDomain: "vue-auth-98113.firebaseapp.com",
-        projectId: "vue-auth-98113",
-        storageBucket: "vue-auth-98113.appspot.com",
-        messagingSenderId: "2023800674",
-        appId: "1:2023800674:web:9c16111f8817967128763b",
-        measurementId: "G-Z0R9GZ3XV9"
-    };
+    apiKey:process.env.API_KEY,
+    authDomain:process.env.AUTH_DOMAIN,
+    projectId:process.env.PROJECT_ID ,
+    storageBucket:process.env.STORAGE_BUCKETS,
+    messagingSenderId:process.env.MESSAGE_SENDING_ID,
+    appId:process.env.APPID,
+    measurementId:process.env.MEASUREMENT_ID
+  };
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
